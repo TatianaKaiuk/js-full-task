988ddc8dafab1c318955265a7c2206ec83d46bec
TatianaKaiuk <persik961243@gmail.com>
2023-01-05 19:27:30 +0200

    calc

--- lesson13/task4/calculator.test.js
+++ lesson13/task4/calculator.test.js
@@ -1,5 +1,22 @@
-import {calc} from './calculator.js';
+import { calc } from './calculator.js';
 
-it ('', () => {
-    
-})
+it('неправильный тип данных', () => {
+  const result = calc([1 + 2]);
+  expect(result).toEqual(null);
+});
+it('should get sum', () => {
+  const result = calc('1 + 2');
+  expect(result).toEqual('1 + 2 = 3');
+});
+it('should get difference', () => {
+  const result = calc('3 - 2');
+  expect(result).toEqual('3 - 2 = 1');
+});
+it('should get division', () => {
+  const result = calc('6 / 2');
+  expect(result).toEqual('6 / 2 = 3');
+});
+it('should get multiplication', () => {
+  const result = calc('6 * 2');
+  expect(result).toEqual('6 * 2 = 12');
+});

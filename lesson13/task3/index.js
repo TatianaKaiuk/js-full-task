78212703b81f0805f43837fa7fae4849195ffc35
TatianaKaiuk <persik961243@gmail.com>
2023-01-05 18:35:30 +0200

    .

--- lesson13/task3/index.js
+++ lesson13/task3/index.js
@@ -1,4 +1,7 @@
 export const getMinSquaredNumber = arr => {
+    if (!Array.isArray(arr) || arr.length === 0) {
+        return null;
+    }
 const square = arr.map(num => (num * num));
 return Math.min(...square);
 }

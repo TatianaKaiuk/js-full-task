d574c8b0a6dab9f853cb8870d1409504bd3f0d8f
TatianaKaiuk <persik961243@gmail.com>
2023-01-28 12:29:18 +0200

    .

---
+++ lesson21/task2/index.js
@@ -0,0 +1,13 @@
+export function getTitleElement() {
+  const titleElem = document.querySelector('.title');
+  console.dir(titleElem);
+  return titleElem;
+}
+//console.dir(getTitleElement());
+
+export function getInputElement() {
+  const inputElem = document.querySelector('input');
+  console.dir(inputElem);
+  return inputElem;
+}
+//console.dir(inputElem)

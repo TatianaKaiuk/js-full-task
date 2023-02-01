79d1f70ca4984c108f34047e3a959b4454c74900
TatianaKaiuk <persik961243@gmail.com>
2023-02-01 10:08:30 +0200

    button

---
+++ lesson22/task1/index.js
@@ -0,0 +1,37 @@
+const divElem = document.querySelector('.rect_div');
+const pElem = document.querySelector('.rect_p');
+const spanElem = document.querySelector('.rect_span');
+const eventsListElem = document.querySelector('.events-list');
+
+const logTarget = (text, color) => {
+  eventsListElem.innerHTML += `<span style="color: ${color}; margin-left: 8px;">${text}</span>`;
+};
+const logGreenDiv = logTarget.bind(null, 'DIV', 'green');
+const logGreenP = logTarget.bind(null, 'P', 'green');
+const logGreenSpan = logTarget.bind(null, 'SPAN', 'green');
+
+const logGreyDiv = logTarget.bind(null, 'DIV', 'grey');
+const logGreyP = logTarget.bind(null, 'P', 'grey');
+const logGreySpan = logTarget.bind(null, 'SPAN', 'grey');
+
+divElem.addEventListener('click', logGreyDiv, true);
+divElem.addEventListener('click', logGreenDiv);
+pElem.addEventListener('click', logGreyP, true);
+pElem.addEventListener('click', logGreenP);
+
+spanElem.addEventListener('click', logGreySpan, true);
+spanElem.addEventListener('click', logGreenSpan);
+
+//  для очистки поля
+const clearEl = document.querySelector('.clear-btn');
+function clear() {
+  eventsListElem.innerHTML = '';
+}
+clearEl.addEventListener('click', clear);
+//
+const remove = document.querySelector('.remove-handlers-btn');
+
+function removed() {
+  eventsListElem.onclick = null;
+}
+remove.addEventListener('click', removed);

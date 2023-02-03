9d81a678e3f00f27ddcb48d26baea90a2a9456b3
TatianaKaiuk <persik961243@gmail.com>
2023-02-03 08:44:04 +0200

    .

--- lesson23/task1/index.js
+++ lesson23/task1/index.js
@@ -1,70 +1,46 @@
-// так можно получить данные формы - ВАРИАНТ 1:
-// eslint-disable-next-line no-undef
-// const formFields = [...new FormData(formElem)];
-// formFields => [["email", "значение поля email"], ["password", "значение поля password"]]
-
-// const formData = formFields.reduce(function (acc, formField) {
-// const prop = formField[0]; // здесь "name" инпута
-// const value = formField[1]; // здесь "value" инпута
-// если использовать деструктуризацию, то можно предыдущие 2 строки записать короче
-// const [prop, value] = formField;
-// return {
-// используем оператор расширения, чтобы в acc добвить свойства все предыдущих итераций
-// ...acc,
-// используем вычислимое свойство объекта, чтобы добавить в аккумулятор новое свойство
-//     [prop]: value,
-//   };
-// }, {});
-
-// более простой формат считывания формы - ВАРИАНТ 2:
-// const formData = Object.fromEntries(new FormData(formElem));
-
-const emailInputElem = document.querySelector(`#email`); // получаем ссылки
+const emailInputElem = document.querySelector(`#email`);
 const passwordInputElem = document.querySelector(`#password`);
 
 const emailErrorElem = document.querySelector(`.error-text_email`);
 const passwordErrorElem = document.querySelector(`.error-text_password`);
 
-const isRequired = (value) => (value ? undefined : `Required`); // пишем ошибки и проверку
+const isRequired = (value) => (value ? undefined : `Required`);
 const isEmail = (value) =>
-  value.includes(`@`) ? undefined : `Should be an email`; // если проверка проходит, ничего не происходит, если нет - ошибку выбевает
+  value.includes(`@`) ? undefined : `Should be an email`;
 
 const validatorsByField = {
-  // чтобы код в дальнейшем не повторялся, указываем в обьекте,
-  email: [isRequired, isEmail], // какие проверку какому полю нужны, в виде массива с именем функции
+  email: [isRequired, isEmail],
   password: [isRequired],
 };
 const validate = (fieldName, value) => {
-  // функция которая принимает имя поля и его значение
-  const validators = validatorsByField[fieldName]; // подставляем с обьекта значения
-  return validators // абра кадабра
+  const validators = validatorsByField[fieldName];
+  return validators
     .map((validator) => validator(value))
     .filter((errorText) => errorText)
     .join(`, `);
 };
 
 const onEmailChange = (event) => {
-  // указываем аргументы для функции validate
-  const errorText = validate(`email`, event.target.value); // email и его значение
-  emailErrorElem.textContent = errorText; // подставляем текст
+  const errorText = validate(`email`, event.target.value);
+  emailErrorElem.textContent = errorText;
 };
 
 const onPasswordChange = (event) => {
-  // указываем аргументы для функции validate
-  const errorText = validate(`password`, event.target.value); // email и его значение
-  passwordErrorElem.textContent = errorText; // подставляем текст
+  const errorText = validate(`password`, event.target.value);
+  passwordErrorElem.textContent = errorText;
 };
 
-emailInputElem.addEventListener('input', onEmailChange); // добавляем обработчик
+emailInputElem.addEventListener('input', onEmailChange);
 passwordInputElem.addEventListener('input', onPasswordChange);
 
-const formElem = document.querySelector('.login-form'); // получаем доступ к элементу
+const formElem = document.querySelector('.login-form');
 
 const onFormSubmit = (event) => {
-  event.preventDefault(); // отменяем настройки браузера по умолчанию
-  const formData = [...new FormData(formElem)] // получаем все введенные данные
-    .reduce((acc, [field, value]) => ({ ...acc, [field]: value }), {});
-  alert(JSON.stringify(formData)); // выводим данные
+  event.preventDefault();
+  const formData = [...new FormData(formElem)].reduce(
+    (acc, [field, value]) => ({ ...acc, [field]: value }),
+    {}
+  );
+  alert(JSON.stringify(formData));
 };
-
 formElem.addEventListener('submit', onFormSubmit);

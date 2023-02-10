764a4224a2db015f687ae8e1c46440ac70db14c9
TatianaKaiuk <persik961243@gmail.com>
2023-02-10 10:59:41 +0200

    todo-list

--- lesson23/task3-todo-list/index.js
+++ lesson23/task3-todo-list/index.js
@@ -1,48 +1,27 @@
 const tasks = [
-  { text: 'Buy milk', done: 'false' },
-  { text: 'Pick up Tom from airport', done: 'false' },
-  { text: 'Visit party', done: 'false' },
-  { text: 'Visit doctor', done: 'true' },
-  { text: 'Buy meat', done: 'false' },
+  { text: 'Buy milk', done: false, id: 'one' },
+  { text: 'Pick up Tom from airport', done: false, id: 'two' },
+  { text: 'Visit party', done: false, id: 'three' },
+  { text: 'Visit doctor', done: true, id: 'four' },
+  { text: 'Buy meat', done: true, id: 'five' },
 ];
 
-tasks.forEach(
-  (el) => (el.id = Math.floor(Math.random() * (100 - 1) + 1).toString())
-);
-
 const listElem = document.querySelector('.list');
-const inputElem = document.querySelector('.task-input');
-const listItemElem = document.createElement('li');
-
-const newList = (event) => {
-  const inputValue = event.target.value;
-  tasks.push({ text: inputValue });
-  // listItemElem.append(inputValue);
-};
-
-inputElem.addEventListener(`change`, newList);
-
-const checkbox = document.createElement('input');
 
 const renderTasks = (tasksList) => {
   const tasksElems = tasksList
-    .sort((a, b) => b.done - a.done)
+    .sort((a, b) => a.done - b.done)
     .map(({ text, done, id }) => {
       const listItemElem = document.createElement('li');
       listItemElem.classList.add('list__item');
       const checkbox = document.createElement('input');
       checkbox.setAttribute('type', 'checkbox');
-      checkbox.classList.add('list__item-checkbox');
-
       checkbox.dataset.id = id;
-      //  checkbox.checked = done;
-
-      // if (done) {
-      //   listItemElem.classList.add('list__item_done');
-      // }
-
-      // checkbox.addEventListener('change', chen);
-
+      checkbox.checked = done;
+      checkbox.classList.add('list__item-checkbox');
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
       listItemElem.append(checkbox, text);
 
       return listItemElem;
@@ -53,48 +32,40 @@ const renderTasks = (tasksList) => {
 
 renderTasks(tasks);
 
-// function chen() {
-//   listItemElem.classList.add('list__item_done');
-// }
-//  checkbox.addEventListener('change', chen);
+// put your code here
+const createBtnElem = document.querySelector('.create-task-btn');
+const inputElem = document.querySelector('.task-input');
+const listItemElem = document.createElement('li');
+
+function newList() {
+  const inputValue = inputElem.value;
+
+  if (inputValue.length === 0) {
+    return null;
+  }
+  tasks.push({
+    text: inputValue,
+    done: false,
+    id: Math.floor(Math.random() * (100 - 1) + 1).toString(),
+  });
+
+  inputElem.value = '';
+  listElem.innerHTML = '';
+  renderTasks(tasks);
+}
+
+createBtnElem.addEventListener('click', newList);
 
 const clickOnCheckbox = (event) => {
   const isCheckbox = event.target.classList.contains('list__item-checkbox');
   if (!isCheckbox) {
     return;
   }
-  const idNumber = event.target.dataset.id;
-  // const elemList = tasks.find(idNumber);
-  const elemId = document.getElementById(idNumber);
-  elemId.done = true;
-  listItemElem.classList.add('list__item_done');
+
+  const taskData = tasks.find((task) => task.id === event.target.dataset.id);
+  Object.assign(taskData, { done: event.target.checked });
+  listElem.innerHTML = '';
+  renderTasks(tasks);
 };
 
-listItemElem.addEventListener(`change`, clickOnCheckbox);
-
-// put your code here
-const createBtnElem = document.querySelector('.create-task-btn');
-
-const getLi = (event) => {
-  const textInput = event.target.value;
-  listItemElem.classList.add('list__item');
-  checkbox.setAttribute('type', 'checkbox');
-
-  checkbox.classList.add('list__item-checkbox');
-
-  listItemElem.append(checkbox, textInput);
-
-  return listItemElem;
-};
-
-const getNewText = () => {
-  if (listItemElem.length === 0) {
-    return;
-  }
-  listElem.append(listItemElem);
-};
-inputElem.addEventListener(`change`, getLi);
-createBtnElem.addEventListener('click', getNewText);
-createBtnElem.addEventListener('click', function () {
-  inputElem.value = '';
-});
+listElem.addEventListener(`click`, clickOnCheckbox);

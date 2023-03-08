7360192631ab929a1b8f0779a49356cfcb967396
TatianaKaiuk <persik961243@gmail.com>
2023-03-08 13:29:54 +0200

    .

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -27,7 +27,7 @@ const onImageLoaded = (error, data) => {
   sizeElem.textContent = `${width} x ${height}`;
 };
 
-// addImage(imgSrc, onImageLoaded);
+ addImage(imgSrc, onImageLoaded);
 
 // examples
 // addImage(

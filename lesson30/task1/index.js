2cf2a92a1441bf547f3ed3a16306c5f6bd7925fa
TatianaKaiuk <persik961243@gmail.com>
2023-03-09 13:05:18 +0200

    .

---
+++ lesson30/task1/index.js
@@ -0,0 +1,26 @@
+const addImageV2 = (url) => {
+  const p = new Promise((resolve, reject) => {
+    const img = document.createElement('img');
+    img.setAttribute('alt', 'User avatar');
+    img.src = url;
+
+    const pageElem = document.querySelector('.page');
+    pageElem.append(img);
+
+    const onImageLoaded = () => {
+      const { width, height } = img;
+      resolve({ width, height });
+    };
+
+    const onImageLoadError = () => reject(new Error('Image load failed'));
+
+    img.addEventListener('load', onImageLoaded);
+
+    img.addEventListener('error', onImageLoadError);
+  });
+  return p;
+};
+const url = 'https://server.com/image.png';
+const resultPromise = addImageV2(url);
+resultPromise.then((data) => console.log(data));
+resultPromise.catch((error) => console.log(error));

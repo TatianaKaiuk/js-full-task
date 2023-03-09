546897da9b34fee63120cbb6d9611a6c5c398d93
TatianaKaiuk <persik961243@gmail.com>
2023-03-09 13:07:29 +0200

    .

--- lesson30/task1/index.js
+++ lesson30/task1/index.js
@@ -1,4 +1,4 @@
-const addImageV2 = (url) => {
+export const addImageV2 = (url) => {
   const p = new Promise((resolve, reject) => {
     const img = document.createElement('img');
     img.setAttribute('alt', 'User avatar');

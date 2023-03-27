57c4062eb86e1721ca3b9a591709c206623e92bc
TatianaKaiuk <persik961243@gmail.com>
2023-03-27 11:22:29 +0300

    .

--- lesson34/login-form-server/scripts/index.js
+++ lesson34/login-form-server/scripts/index.js
@@ -1,14 +1,29 @@
 import { createData, getData } from './dataGetaway.js';
 
-// const emailInputElem = document.querySelector(`#email`);
-// const passwordInputElem = document.querySelector(`#password`);
-// const nameInputElem = document.querySelector(`#name`);
+const emailInputElem = document.querySelector(`#email`);
+const passwordInputElem = document.querySelector(`#password`);
+const nameInputElem = document.querySelector(`#name`);
 const btnElem = document.querySelector(`.submit-button`);
 const formElem = document.querySelector(`.login-form`);
 const imputElem = document.querySelector(`.form-input`);
-btnElem.setAttribute('disabled', Boolean);
 
+// btnElem.setAttribute('disabled', true);
 const formData = Object.fromEntries(new FormData(formElem));
+
+if (formElem.reportValidity()) {
+  btnElem.removeAttribute('disabled');
+} else {
+  btnElem.setAttribute('disabled', true);
+}
+
+// const arrayElem = [emailInputElem, passwordInputElem, nameInputElem];
+// arrayElem.forEach((el) => {
+//   if (el.reportValidity()) {
+//     btnElem.removeAttribute('disabled');
+//   } else {
+//     btnElem.setAttribute('disabled', true);
+//   }
+// });
 
 // const changeElem = (data) => {
 //   data.array.forEach((element) => {
@@ -20,20 +35,19 @@ const formData = Object.fromEntries(new FormData(formElem));
 // changeElem(formData);
 // imputElem.addEventListener('change', changeElem(formData));
 
-imputElem.addEventListener(
-  'submit',
-  function () {
-    imputElem.reportValidity();
-  },
-  false
-);
-
-
+// imputElem.addEventListener(
+//   'submit',
+//   function () {
+//     imputElem.reportValidity();
+//   },
+//   false
+// );
 
 const sendingData = () => {
-  createData(formData);
+  createData(formData).then(() => console.log(formData));
   formElem.reset('');
 };
+// createData(formData).then(() => console.log(formData))
 
 btnElem.addEventListener('click', sendingData);
 
@@ -42,4 +56,4 @@ const onFormSubmit = (event) => {
   getData().then((resolt) => alert(JSON.stringify(resolt)));
 };
 
-btnElem.addEventListener(`submit`, onFormSubmit);
+btnElem.addEventListener(`click`, onFormSubmit);
